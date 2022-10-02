c61aae2c21b364cf3ab1eb06001184fe478ac50b
Autumn Sprout <jotasprout@gmail.com>
2022-10-02 17:06:33 -0400

    getting methods to work

---
+++ AbilityRoll.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class AbilityRoll 
+{
+    public int sixSidedRoll;
+    public int abilityRoll;
+
+    public AbilityRoll()
+    {
+       rollForAbility();
+    }
+
+    // roll one six-sided die
+    public int rollSixSided()
+    {
+        Random objGenerator = new Random();
+        int randomSixSidedNum = objGenerator.nextInt(6);
+        // for (int iCount = 0; iCount < 10; iCount++)
+        // {
+        //     randomSixSidedNum = objGenerator.nextInt(6);
+        System.out.println("Random six-sided dice roll = " + randomSixSidedNum + ".");
+        // }
+        return randomSixSidedNum;
+    }
+
+    // roll for abilities
+    // Simulation of rolling three six-sided die for each ability
+    public int rollForAbility()
+    {
+        for (int die = 0;  die <= 2; die++){
+            sixSidedRoll = rollSixSided();
+            abilityRoll += sixSidedRoll;
+        }
+        System.out.println("Ability roll = " + abilityRoll + ".");
+        return abilityRoll;
+    } 
+
+}
--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
-public class Main {
+//import java.util.*;
+
+public class Main 
+
+{
+    public static void main (String args[]) {
+
+        AbilityRoll strength = new AbilityRoll();
+
+        System.out.println("Strength is " + strength.abilityRoll + ".");
+    }
+
+
     
 }
