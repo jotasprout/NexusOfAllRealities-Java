eac73c3df8f7f7f7472928938ead8b63bebfaaf9
Jay Sprout <jotasprout@gmail.com>
2022-11-27 11:27:36 -0500

    abilities are in their own class

--- Abilities.java
+++ Abilities.java
@@ -1,3 +1,49 @@
 public class Abilities {
-    
+
+    static Character rollForAbilities(Character aChar) {
+
+        Character myChar = new Character();
+
+        myChar = aChar;
+
+        String[] abilities = { "strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma" };
+
+        for (String ability : abilities) {
+            System.out.println("Rolling for " + ability + " ...");
+            AbilityRoll thisRoll = new AbilityRoll();
+
+            switch (ability) {
+                case "strength":
+                    System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
+                    myChar.strength = thisRoll.abilityRoll;
+                    break;
+                case "dexterity":
+                    System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
+                    myChar.dexterity = thisRoll.abilityRoll;
+                    break;
+                case "constitution":
+                    System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
+                    myChar.constitution = thisRoll.abilityRoll;
+                    break;
+                case "intelligence":
+                    System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
+                    myChar.intelligence = thisRoll.abilityRoll;
+                    break;
+                case "wisdom":
+                    System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
+                    myChar.wisdom = thisRoll.abilityRoll;
+                    break;
+                case "charisma":
+                    System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
+                    myChar.charisma = thisRoll.abilityRoll;
+                    break;
+                default:
+                    System.out.println("You need better dice.");
+                    // need a custom exception here
+            }
+
+        }
+
+        return myChar;
+    }
 }
--- Main.java
+++ Main.java
@@ -15,44 +15,7 @@ public class Main
 
                 System.out.println("Let's see what " + myChar.name + " is good for.");
 
-                // SHOULDN'T THE FOLLOWING BE A METHOD?
-
-                String[] abilities = { "strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma" };
-
-                for (String ability : abilities) {
-                        System.out.println("Rolling for " + ability + " ...");
-                        AbilityRoll thisRoll = new AbilityRoll();
-
-                        switch (ability) {
-                                case "strength":
-                                        System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
-                                        myChar.strength = thisRoll.abilityRoll;
-                                        break;
-                                case "dexterity":
-                                        System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
-                                        myChar.dexterity = thisRoll.abilityRoll;
-                                        break;
-                                case "constitution":
-                                        System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
-                                        myChar.constitution = thisRoll.abilityRoll;
-                                        break;
-                                case "intelligence":
-                                        System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
-                                        myChar.intelligence = thisRoll.abilityRoll;
-                                        break;
-                                case "wisdom":
-                                        System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
-                                        myChar.wisdom = thisRoll.abilityRoll;
-                                        break;
-                                case "charisma":
-                                        System.out.println(ability + " is " + thisRoll.abilityRoll + ".");
-                                        myChar.charisma = thisRoll.abilityRoll;
-                                        break;
-                                default:
-                                        System.out.println("You need better dice.");
-                                        // need a custom exception here
-                        }
-                }
+                myChar = Abilities.rollForAbilities(myChar);
 
                 myChar.race = Race.chooseRace();
 
